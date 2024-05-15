17c7ee94f52dc7a184035b4843f13b4e062b0728
ChienHuang0818 <betty556611@gmail.com>
2024-05-15 13:01:15 +1000

    navbar

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -4,7 +4,6 @@ import '../../App.css';
 
 function Navbar() {
   const [navActive, setNavActive] = useState(false);
-  const [showNav, setShowNav] = useState(true);
 
   const toggleNav = () => {
     setNavActive(!navActive);
@@ -16,11 +15,8 @@ function Navbar() {
 
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth <= 500) {
-        closeMenu();
-        setShowNav(false); // 隱藏導航欄和漢堡菜單
-      } else {
-        setShowNav(true); // 顯示導航欄和漢堡菜單
+      if (window.innerWidth > 1200) {
+        setNavActive(false);
       }
     };
 
@@ -36,95 +32,91 @@ function Navbar() {
 
   return (
     <nav className={`navbar ${navActive ? "active" : ""}`}>
-      {showNav && (
-        <>
-          <div>
-            <img src="./img/sarah.png" alt="ChienHuang" width="150px" height="120px" />
-          </div>
-          <div
-            className={`nav__hamburger ${navActive ? "active" : ""}`}
-            onClick={toggleNav}
-          >
-            <span className="nav__hamburger__line"></span>
-            <span className="nav__hamburger__line"></span>
-            <span className="nav__hamburger__line"></span>
-          </div>
-          <div className={`navbar--items ${navActive ? "active" : ""}`}>
-            <ul>
-              <li>
-                <Link
-                  onClick={closeMenu}
-                  activeClass="navbar--active-content"
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={500}
-                  to="heroSection"
-                  className="navbar--content"
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={closeMenu}
-                  activeClass="navbar--active-content"
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={500}
-                  to="AboutMe"
-                  className="navbar--content"
-                >
-                  About Me
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={closeMenu}
-                  activeClass="navbar--active-content"
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={500}
-                  to="mySkills"
-                  className="navbar--content"
-                >
-                  My Skills
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={closeMenu}
-                  activeClass="navbar--active-content"
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={500}
-                  to="MyPortfolio"
-                  className="navbar--content"
-                >
-                  Portfolio
-                </Link>
-              </li>
-              <li>
-                <Link
-                  onClick={closeMenu}
-                  activeClass="navbar--active-content"
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={500}
-                  to="Contact"
-                  className="navbar--content contact-section"
-                >
-                  Contact Me
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </>
-      )}
+      <div>
+        <img src="./img/sarah.png" alt="ChienHuang" width="150px" height="120px" />
+      </div>
+      <div
+        className={`nav__hamburger ${navActive ? "active" : ""}`}
+        onClick={toggleNav}
+      >
+        <span className="nav__hamburger__line"></span>
+        <span className="nav__hamburger__line"></span>
+        <span className="nav__hamburger__line"></span>
+      </div>
+      <div className={`navbar--items ${navActive ? "active" : ""}`}>
+        <ul>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="heroSection"
+              className="navbar--content"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="AboutMe"
+              className="navbar--content"
+            >
+              About Me
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="mySkills"
+              className="navbar--content"
+            >
+              My Skills
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="MyPortfolio"
+              className="navbar--content"
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar--active-content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="Contact"
+              className="navbar--content contact-section"
+            >
+              Contact Me
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }

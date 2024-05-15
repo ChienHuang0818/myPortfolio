fb474fd77ccc6a8cb2c567ace736ee2636e47b61
ChienHuang0818 <betty556611@gmail.com>
2024-05-15 13:38:49 +1000

    add cv

---
+++ src/Pages/Home/CV.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css'; 
+
+const CV = () => {
+  return (
+    <div className="cv-container">
+      <img src="/img/cv.pdf" alt="CV" className="cv-image" />
+    </div>
+  );
+};
+
+export default CV;
--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 import '../../App.css'; 
 
 function Navbar() {
   const [navActive, setNavActive] = useState(false);
+  const [showNavItems, setShowNavItems] = useState(true);
 
   const toggleNav = () => {
     setNavActive(!navActive);
@@ -15,8 +16,11 @@ function Navbar() {
 
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth > 1200) {
-        setNavActive(false);
+      if (window.innerWidth <= 500) {
+        closeMenu();
+        setShowNavItems(false); // 隱藏導航欄項目
+      } else {
+        setShowNavItems(true); // 顯示導航欄項目
       }
     };
 
@@ -43,80 +47,93 @@ function Navbar() {
         <span className="nav__hamburger__line"></span>
         <span className="nav__hamburger__line"></span>
       </div>
-      <div className={`navbar--items ${navActive ? "active" : ""}`}>
-        <ul>
-          <li>
-            <Link
-              onClick={closeMenu}
-              activeClass="navbar--active-content"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="heroSection"
-              className="navbar--content"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={closeMenu}
-              activeClass="navbar--active-content"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="AboutMe"
-              className="navbar--content"
-            >
-              About Me
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={closeMenu}
-              activeClass="navbar--active-content"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="mySkills"
-              className="navbar--content"
-            >
-              My Skills
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={closeMenu}
-              activeClass="navbar--active-content"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="MyPortfolio"
-              className="navbar--content"
-            >
-              Portfolio
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={closeMenu}
-              activeClass="navbar--active-content"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="Contact"
-              className="navbar--content contact-section"
-            >
-              Contact Me
-            </Link>
-          </li>
-        </ul>
-      </div>
+      {showNavItems && (
+        <div className={`navbar--items ${navActive ? "active" : ""}`}>
+          <ul>
+            <li>
+              <Link
+                onClick={closeMenu}
+                activeClass="navbar--active-content"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="heroSection"
+                className="navbar--content"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={closeMenu}
+                activeClass="navbar--active-content"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="AboutMe"
+                className="navbar--content"
+              >
+                About Me
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={closeMenu}
+                activeClass="navbar--active-content"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="mySkills"
+                className="navbar--content"
+              >
+                My Skills
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={closeMenu}
+                activeClass="navbar--active-content"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="MyPortfolio"
+                className="navbar--content"
+              >
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={closeMenu}
+                activeClass="navbar--active-content"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="Contact"
+                className="navbar--content contact-section"
+              >
+                Contact Me
+              </Link>
+            </li>
+            <li>
+              <a
+                href="./img/CV.png"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="navbar--content"
+                onClick={closeMenu}
+              >
+                CV
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 }

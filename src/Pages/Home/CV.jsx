e095e625ff24ae9a6086b1f74a26c2c9927b921a
ChienHuang0818 <betty556611@gmail.com>
2024-06-01 21:36:00 +1000

    update cv

--- src/Pages/Home/CV.jsx
+++ src/Pages/Home/CV.jsx
@@ -3,15 +3,16 @@ import '../../App.css';
 
 const CV = () => {
   return (
-    <div className="cv-container">
+    <div className="cv-container" style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
       <iframe 
         src="/img/cv.pdf"  
         title="CV"
         className="cv-iframe"
-        style={{ width: '100%', height: '100vh', border: 'none' }}
+        style={{ width: '100%', height: '100%', border: 'none' }}
       />
     </div>
   );
 };
 
 export default CV;
+

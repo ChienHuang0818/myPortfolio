d90caf7473471105e367001238af6d2104767ffe
ChienHuang0818 <betty556611@gmail.com>
2025-10-13 19:38:17 +1000

    modify navbar

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
-import '../../App.css'; 
+import "../../App.css";
 
 function Navbar() {
   const [navActive, setNavActive] = useState(false);
@@ -31,13 +31,12 @@ function Navbar() {
 
   return (
     <nav className={`navbar ${navActive ? "active" : ""}`}>
-      <div>
-        <img src="/img/sarah.png" alt="ChienHuang" width="150px" height="120px" />
+      <div className="navbar--logo">
+        <NavLink to="/hero" onClick={closeMenu}>
+          <img src="/img/sarah.png" alt="ChienHuang" width="80px" height="70px" />
+        </NavLink>
       </div>
-      <div
-        className={`nav__hamburger ${navActive ? "active" : ""}`}
-        onClick={toggleNav}
-      >
+      <div className={`nav__hamburger ${navActive ? "active" : ""}`} onClick={toggleNav}>
         <span className="nav__hamburger__line"></span>
         <span className="nav__hamburger__line"></span>
         <span className="nav__hamburger__line"></span>
@@ -45,56 +44,32 @@ function Navbar() {
       <div className={`navbar--items ${navActive ? "active" : ""}`}>
         <ul>
           <li>
-            <NavLink
-              to="/"
-              className="navbar--content"
-              onClick={closeMenu}
-            >
+            <NavLink to="/hero" className="navbar--content" onClick={closeMenu}>
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="/about-me"
-              className="navbar--content"
-              onClick={closeMenu}
-            >
+            <NavLink to="/about-me" className="navbar--content" onClick={closeMenu}>
               About Me
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="/my-skills"
-              className="navbar--content"
-              onClick={closeMenu}
-            >
+            <NavLink to="/my-skills" className="navbar--content" onClick={closeMenu}>
               My Skills
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="/my-portfolio"
-              className="navbar--content"
-              onClick={closeMenu}
-            >
+            <NavLink to="/my-portfolio" className="navbar--content" onClick={closeMenu}>
               Portfolio
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="/contact"
-              className="navbar--content contact-section"
-              onClick={closeMenu}
-            >
+            <NavLink to="/contact" className="navbar--content contact-section" onClick={closeMenu}>
               Contact Me
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="/cv"
-              className="navbar--content"
-              onClick={closeMenu}
-            >
+            <NavLink to="/cv" className="navbar--content" onClick={closeMenu}>
               CV
             </NavLink>
           </li>
